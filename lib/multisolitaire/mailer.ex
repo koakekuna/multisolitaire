6924a0fa5803785d3a778cff94f1da4951fbfde4
Koa Kekuna <kkekuna@dudnyk.com>
2024-09-23 21:02:29 -0400

    feat: generate new phoenix app

---
+++ lib/multisolitaire/mailer.ex
@@ -0,0 +1,3 @@
+defmodule Multisolitaire.Mailer do
+  use Swoosh.Mailer, otp_app: :multisolitaire
+end
---
+++ lib/multisolitaire_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule MultisolitaireWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use MultisolitaireWeb, :controller` and
+  `use MultisolitaireWeb, :live_view`.
+  """
+  use MultisolitaireWeb, :html
+
+  embed_templates "layouts/*"
+end
